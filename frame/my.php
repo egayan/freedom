a21f7b0c9e14cd3f636482f235591fb24fb72fa5
kouki000111 <2201200@s.asojuku.ac.jp>
2023-11-08 11:54:20 +0900

    Update my.php

--- frame/my.php
+++ frame/my.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>マイページ</title>
-    <link href="css/my.css" rel="stylesheet">
+    <link href="style/my.css" rel="stylesheet">
 </head>
 <body><div class="wrap">
 
