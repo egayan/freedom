c2f8b728493d19d45e5026ab3ab1509ad424d632
munra46 <148170471+munra46@users.noreply.github.com>
2023-11-10 12:20:59 +0900

    a

--- frame/detail.php
+++ frame/detail.php
@@ -1,7 +1,6 @@
 <?php session_start(); ?>
 <?php require 'db-conect.php'; ?>
 <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
 <?php
 $move_id=$_GET['id'];
 $pdo=new PDO($connect,USER,PASS);
@@ -15,28 +14,24 @@ $movie = $stmt->fetch(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="styles/detail.css">
 <title><?php echo $movie['shohin_mei'] ; ?>詳細</title>
 </head>
-<body> 
-<div class="wrap">
-<div class="mei"><?php echo $movie['shohin_mei']; ?></div>
-        <div class="syou"><img src="image/<?php echo $movie['image']; ?>" alt="<?php echo $movie['shohin_mei']; ?>"></div>
-        <div class="ryoukin"><?php echo "料金：",$movie['price'],"円"; ?> </div>
-        <div class="zikan"><?php echo $movie['time']; ?> </div>	
-        <div class="mozi"><?php echo $movie['explanation']; ?> </div>
+<body>
+<?php echo $movie['shohin_mei']; ?>
+        <img src="image/<?php echo $movie['image']; ?>" alt="<?php echo $movie['shohin_mei']; ?>">
         <?php
         echo '<form action="cart-insert.php" method="post">';
         echo '<input type="hidden" name="id" value="',$movie['shohin_id'],'">';
         echo '<input type="hidden" name="name" value="',$movie['shohin_mei'],'">';
         echo '<input type="hidden" name="price" value="',$movie['price'],'">';
         echo '<input type="hidden" name="image" value="',$movie['image'],'">';
-        echo '<div class="kato"><p><input type="submit" value="カートに追加"></p></div>';
-        echo '</form>';
-        echo '<form action="getail.php" method="post">';//レビュー
-        echo '<div class="review"><p><input type="submit" value="レビュー評価を見る"></p></div>';
+        echo '<p><input type="submit" value="カートに追加"></p>';
         echo '</form>';
         ?>
-
+        <form action="review" method="post">
+        <input type="hidden" name="shohin_id" value="<?php echo $movie['shohin_id']; ?>">
+            <input type="submit" value="レビューを見る">
+        </form>
         </body>
+<?php require 'menu.php'; ?>
 </html>
