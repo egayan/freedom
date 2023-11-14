2a7fc4a901b80d2a169334db5cc82c54a512fee7
Syuuseiuno <syu.uk72@gmail.com>
2023-11-14 11:41:35 +0900

    カート　手直し

--- frame/cart-show.php
+++ frame/cart-show.php
@@ -1,26 +1,29 @@
+<link rel="stylesheet" href="styles/cart-show.css">
 <?php
 if(!empty($_SESSION['movie'])){
-    echo '<table>';
+    echo '<table border="1"  class="iti">';
     echo '<tr><th>購入商品</th><th>販売価格</th>';
-    echo '<th>削除</th><th></th><th></th><th></th></tr>';
+    echo '<th>削除</th><th></th></tr>';
     $total=0;
     foreach($_SESSION['movie'] as $id=>$product){
         echo '<tr>';
         ?>
-        <img src="image/<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>">
         <?php
-        echo '<td><a href="detail.php?id=',$id,'">',$product['name'],'</a></td>';
+         echo '<td  width="500px"><img src="image/' . $product['image'] . '" alt="' . $product['name'] . '">';
+        echo '<a href="detail.php?id=',$id,'">',$product['name'],'</a></td>';
         echo '<td>',$product['price'],'</td>';
         $total+=$product['price'];
-        echo '<td><a href="cart-delete.php?id=',$id,'">削除</a></td>';
+        echo '<td width="50px"><a href="cart-delete.php?id=',$id,'">削除</a></td>';
         echo '</tr>';
     }
-    echo '<tr><td>合計</td><td></td><td></td><td>',$total,'</td><td></td></tr>';
+    echo '<tr><td>合計</td> <td></td> <td></td> <td>',$total,'</td></tr>';
     echo '</table>';
     ?>
+    </div>
     <form action=purchase.php method="get">
-        <input type="submit" value="購入">
-    </form>
+    <div class="kakutei">  <input type="submit" value="購入" class="button">
+</div>
+</form>
     <?php
 }else{
     echo 'カートに商品がありません';
